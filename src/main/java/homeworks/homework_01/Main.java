f87baac302e40e348d25f4983a4948118d872c8a
«Andrew» <gepatytich@gmail.com>
2024-05-18 02:19:04 +0700

    added a homework assignment

---
+++ src/main/java/homeworks/homework_01/Main.java
@@ -0,0 +1,10 @@
+package homeworks.homework_01;
+
+public class Main {
+    public static void main(String[] args) {
+        new ServerWindow();
+        new ClientGUI();
+        new ClientGUI();
+        System.out.println("Method main() is over");
+    }
+}
