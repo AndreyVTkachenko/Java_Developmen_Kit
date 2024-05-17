49ef31580eec4a961d11f2d48285ebf5c9021ea0
«Andrew» <gepatytich@gmail.com>
2024-05-18 01:44:15 +0700

    added seminar 01

---
+++ src/main/java/lessons/lesson_01/Main.java
@@ -0,0 +1,8 @@
+package lessons.lesson_01;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameWindow();
+        System.out.println("Method main() is over");
+    }
+}
