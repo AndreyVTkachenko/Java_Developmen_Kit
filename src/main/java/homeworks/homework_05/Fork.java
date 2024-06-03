1d81c89fcd29758c2941f4387b2ae4d93fcd045c
«Andrew» <gepatytich@gmail.com>
2024-06-03 16:55:30 +0700

    added homework 05

---
+++ src/main/java/homeworks/homework_05/Fork.java
@@ -0,0 +1,15 @@
+package homeworks.homework_05;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Fork {
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void take() {
+        lock.lock();
+    }
+
+    public void put() {
+        lock.unlock();
+    }
+}
---
+++ src/main/java/homeworks/homework_05/Main.java
@@ -0,0 +1,11 @@
+package homeworks.homework_05;
+
+public class Main {
+    public static void main(String[] args) {
+//        String[] names = {"Аристотель", "Гераклит", "Платон", "Сократ", "Пифагор"};
+        String[] names = {"Первый", "Второй", "Третий", "Четвёртый", "Пятый"};
+        int eatingCount = 3;
+        Table table = new Table(names, eatingCount);
+        table.startEating();
+    }
+}
